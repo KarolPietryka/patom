b43665deade1fc8932e6fe74c78067aaf3b190e9
Karol Pietryka <karol.pietryka11@gmail.com>
2023-01-04 18:42:11 +0100

    [Add]Add html modifier to handle data type properties

---
+++ patom-platform/src/main/kotlin/pl/patom/service/file/html/transformer/strategy/modifier/HtmlDateModifier.kt
@@ -0,0 +1,20 @@
+package pl.patom.service.file.html.transformer.strategy.modifier
+
+import org.alfresco.repo.copy.CompoundCopyBehaviourCallback
+import org.apache.commons.logging.LogFactory
+import org.springframework.stereotype.Component
+import pl.patom.model.template.content.html.entity.HtmlEntity
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Component("htmlDateModifier")
+class HtmlDateModifier: HTMLModifier {
+    private val logger = LogFactory.getLog(HtmlDateModifier::class.java)
+
+    override fun modify(entityToReplace: HtmlEntity, htmlText: String): String {
+        val df = SimpleDateFormat("yyyy-mm-dd")
+        logger.debug(entityToReplace.acsPropInHtmlNotation + entityToReplace.acsPropValue as? Date )
+        return htmlText.replace(
+            entityToReplace.acsPropInHtmlNotation,
+            entityToReplace.acsPropValue?.let { df.format(it as Date) } ?: "") }
+}
